0cbcaf2962055dbec4c9ecc17c08816b2f752bf4
bisma-im <bismaimran36@gmail.com>
2023-10-12 19:20:55 +0500

    xylophone-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNum){
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded createButton(int soundNum, Color clr){
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(clr),
+        ),
+        onPressed: () {
+          playSound(soundNum);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                createButton(1, Colors.red),
+                createButton(2, Colors.orange),
+                createButton(3, Colors.yellow),
+                createButton(4, Colors.green),
+                createButton(5, Colors.teal),
+                createButton(6, Colors.blue),
+                createButton(7, Colors.purple),
+              ],
+            ),
+          ),
         ),
       ),
     );
